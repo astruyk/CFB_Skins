28478c7272b759bdd4513432494decfaaa4d0801
Anton <Anton@.(none)>
2014-10-29 23:24:39 -0400

    Add kitbag reskin. Update assault pack reskin.

--- src/@CFB_Skins/addons/cfb_skins/vehicles_backpacks_tw.hpp
+++ src/@CFB_Skins/addons/cfb_skins/vehicles_backpacks_tw.hpp
@@ -1,12 +1,25 @@
 class B_AssaultPack_cbr;
-	class CFB_Backpack_Assault_TW : B_AssaultPack_cbr
-	{
-		side = 1;
-		scope = 2;
-		scopeCurator = 2;
-		displayName = "CADPAT TW Assault Backpack";
-		author = "Anton Struyk";
-		
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"\cfb_skins\CADPAT_Backpack_Assault_TW.paa"};
-	};
+class CFB_Backpack_Assault_TW : B_AssaultPack_cbr
+{
+	side = 1;
+	scope = 2;
+	scopeCurator = 2;
+	displayName = "CADPAT TW Assault Backpack";
+	author = "Anton Struyk";
+	
+	hiddenSelections[] = {"camo"};
+	hiddenSelectionsTextures[] = {"\cfb_skins\CADPAT_Backpack_Assault_TW.paa"};
+};
+
+class B_Kitbag_cbr;
+class CFB_Backpack_Kitbag_TW : B_Kitbag_cbr
+{
+	side = 1;
+	scope = 2;
+	scopeCurator = 2;
+	displayName = "CADPAT TW Kitbag";
+	author = "Anton Struyk";
+	
+	hiddenSelections[] = {"camo"};
+	hiddenSelectionsTextures[] = {"\cfb_skins\CADPAT_Backpack_Fast_TW.paa"};
+};
