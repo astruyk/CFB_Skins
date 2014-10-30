ca1ac847b038f7b2cc49077a23fe10388e4518be
Anton <Anton@.(none)>
2014-10-29 23:54:14 -0400

    Follow naming convention for C97A3 sight

--- src/@CFB_Skins/addons/cfb_skins/weapons_sights.hpp
+++ src/@CFB_Skins/addons/cfb_skins/weapons_sights.hpp
@@ -1,6 +1,6 @@
 // TODO doesn't work :(
 class optic_Arco;
-class CFB_CADPAT_C97A3_Sight : optic_Arco
+class CFB_C97A3_Sight : optic_Arco
 {
 	side = 1;
 	scope = 2;
@@ -23,7 +23,7 @@ class arifle_MX_F : arifle_MX_Base_F
 	{
 		class CowsSlot : CowsSlot
 		{
-			compatibleItems[] = {"CFB_CADPAT_C97A3_Sight", "optic_Nightstalker","optic_tws","optic_tws_mg","optic_NVS","optic_DMS","optic_LRPS","optic_SOS","optic_MRCO","optic_Arco","optic_aco","optic_ACO_grn","optic_aco_smg","optic_ACO_grn_smg","optic_hamr","optic_Holosight","optic_Holosight_smg"};
+			compatibleItems[] = {"CFB_C97A3_Sight", "optic_Nightstalker","optic_tws","optic_tws_mg","optic_NVS","optic_DMS","optic_LRPS","optic_SOS","optic_MRCO","optic_Arco","optic_aco","optic_ACO_grn","optic_aco_smg","optic_ACO_grn_smg","optic_hamr","optic_Holosight","optic_Holosight_smg"};
 		};
 	};
 };
