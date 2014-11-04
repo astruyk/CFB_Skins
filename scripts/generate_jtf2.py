1baaa2c9e7aeccf39d721514b6fecec461797612
anton <astruyk@gmail.com>
2014-11-03 20:06:50 -0500

    Consolidate both group and unit update scripts for JTF2 into one script. Remove AR script since we don't have assets for it yet.

--- scripts/generate_jtf2.py
+++ scripts/generate_jtf2.py
@@ -56,3 +56,22 @@ with open(inputFileName, 'r') as inputFile:
 			if shouldBeCommentedOut:
 				outputFile.write("// ");
 			outputFile.write(line);
+
+# Generate equivalent groups for JTF2 units
+inputFileName = "../src/@CFB_Skins/addons/cfb_skins/groups_tw.hpp";
+outputFileName = "../src/@CFB_Skins/addons/cfb_skins/groups_jtf2.hpp";
+
+with open(inputFileName, 'r') as inputFile:
+	with open(outputFileName, 'w') as outputFile:
+		for line in inputFile:
+
+			# Transform the base class name from TW groups to JTF2 groups
+			line = line.replace("CFB_Groups_CADPAT_TW", "CFB_Groups_JTF2");
+			
+			# Change the section names
+			line = line.replace("Infantry (CADPAT TW)", "Infantry (JTF2)");
+			
+			# Change the names of the units
+			line = line.replace("CFB_TW_Soldier", "CFB_JTF2_Soldier");
+
+			outputFile.write(line);
