e2e2f941af5f125e075320ae996273631abeba87
anton <astruyk@gmail.com>
2014-11-26 19:06:03 -0500

    Fix armed LSSV not having correct camo.

--- src/@CFB_Skins/addons/cfb_skins/vehicles_ground.hpp
+++ src/@CFB_Skins/addons/cfb_skins/vehicles_ground.hpp
@@ -11,8 +11,8 @@ class CFB_LSSV_Olive : Offroad_01_base_F
 	hiddenSelectionsTextures[] = { "\cfb_skins\LSSV_Olive.paa", "\cfb_skins\LSSV_Olive.paa" };
 };
 
-class I_G_Offroad_01_armed_F;
-class CFB_LSSV_Olive_Armed : I_G_Offroad_01_armed_F
+class Offroad_01_armed_base_F;
+class CFB_LSSV_Olive_Armed : Offroad_01_armed_base_F
 {
 	side = 1;
 	scope = 2;
@@ -21,5 +21,5 @@ class CFB_LSSV_Olive_Armed : I_G_Offroad_01_armed_F
 	vehicleClass = "CFB_Utility_VehicleClass";
 	displayName = "LSSV (Olive, Armed)";
 	crew = "CFB_TW_Soldier";
-	hiddenSelectionsTextures[] = { "\cfb_skins\LSSV_Olive.paa", "\A3\soft_f\Offroad_01\data\Offroad_01_ext_co.paa" };
+	hiddenSelectionsTextures[] = { "\cfb_skins\LSSV_Olive.paa", "\cfb_skins\LSSV_Olive.paa" };
 };
