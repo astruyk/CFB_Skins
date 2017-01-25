8f27997b51b30e6b1ea1ab39d34c2a6e55e3c178
Anton Struyk <astruyk@gmail.com>
2017-01-25 16:23:45 -0500

    Update to 3.0.1

--- bin/@CFB_Skins/mod.cpp
+++ bin/@CFB_Skins/mod.cpp
@@ -2,6 +2,6 @@ name = "CFB Skins";
 picture = "logo.paa";
 actionName = "Website";
 action = "https://github.com/astruyk/CFB_Skins";
-tooltip = "Canadian Forces Base Skins 3.0.0";
+tooltip = "Canadian Forces Base Skins 3.0.1";
 overview = "Canadian Armed Forces inspired skins for the base ARMA units and vehicles.";
 author = "Anton Struyk";
