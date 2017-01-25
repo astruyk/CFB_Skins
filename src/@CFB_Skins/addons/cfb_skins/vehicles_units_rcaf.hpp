1ae4d29a94d93f8cb1001db08ecc2c08ae00f023
Anton Struyk <astruyk@gmail.com>
2017-01-25 10:29:55 -0500

    Fix issue where skins on CH-147 Chinook weren't showing
    Turns out BIS changed the way that the Huron initializes it's skins so
    that a new override was neeeded.

--- src/@CFB_Skins/addons/cfb_skins/vehicles_units_rcaf.hpp
+++ src/@CFB_Skins/addons/cfb_skins/vehicles_units_rcaf.hpp
@@ -37,6 +37,16 @@ class CFB_Helo_Griffon : I_Heli_light_03_F
 	vehicleClass = "CFB_RCAF_VehicleClass";
 	crew = "CFB_RCAF_Helo_Pilot";
 	hiddenSelectionsTextures[] = { "\cfb_skins\CH146_0.paa" };
+	
+	class TextureSources
+	{
+		class RCAF_Camo
+		{
+			displayName = "RCAF Camo";
+			factions[] = { "BLU_F" };
+			textures[] = { "\cfb_skins\CH146_0.paa" };
+		};
+	};
 };
 
 class I_Heli_light_03_unarmed_F;
@@ -50,6 +60,18 @@ class CFB_Helo_Griffon_Unarmed : I_Heli_light_03_unarmed_F
 	vehicleClass = "CFB_RCAF_VehicleClass";
 	crew = "CFB_RCAF_Helo_Pilot";
 	hiddenSelectionsTextures[] = { "\cfb_skins\CH146_0.paa" };
+	
+	// This vehicle seems to work without this specified - but this is an attempt to
+	// future-proof in case the hiddenSelectionsTextures[] stops working in future versions.
+	class TextureSources
+	{
+		class RCAF_Camo
+		{
+			displayName = "RCAF Camo";
+			factions[] = { "BLU_F" };
+			textures[] = { "\cfb_skins\CH146_0.paa" };
+		};
+	};
 };
 
 class I_Heli_Transport_02_F;
@@ -62,7 +84,19 @@ class CFB_Helo_Cyclone : I_Heli_Transport_02_F
 	faction = "CFB_RCAF";
 	vehicleClass = "CFB_RCAF_VehicleClass";
 	crew = "CFB_RCAF_Helo_Pilot";
-	hiddenSelectionsTextures[] = { "\cfb_skins\CH148_0.paa", "\cfb_skins\CH148_1.paa", "\cfb_skins\CH148_2.paa"};
+	hiddenSelectionsTextures[] = { "\cfb_skins\CH148_0.paa", "\cfb_skins\CH148_1.paa", "\cfb_skins\CH148_2.paa" };
+	
+	// This vehicle seems to work without this specified - but this is an attempt to
+	// future-proof in case the hiddenSelectionsTextures[] stops working in future versions.
+	class TextureSources
+	{
+		class RCAF_Camo
+		{
+			displayName = "RCAF Camo";
+			factions[] = { "BLU_F" };
+			textures[] = { "\cfb_skins\CH148_0.paa", "\cfb_skins\CH148_1.paa", "\cfb_skins\CH148_2.paa" };
+		};
+	};
 };
 
 class B_Heli_Transport_03_F;
@@ -76,6 +110,18 @@ class CFB_Helo_Chinook : B_Heli_Transport_03_F
 	vehicleClass = "CFB_RCAF_VehicleClass";
 	crew = "CFB_RCAF_Helo_Pilot";
 	hiddenSelectionsTextures[] = { "\cfb_skins\CH147_0.paa", "\cfb_skins\CH147_1.paa" };
+
+	// This vehicle vehicle uses init[] script to automatically choose a skin from the texture sources
+	// (overwriting the hiddenSelectionsTextures[]). This forces it to choose one of the custom skins.
+	class TextureSources
+	{
+		class RCAF_Camo
+		{
+			displayName = "RCAF Camo";
+			factions[] = { "BLU_F" };
+			textures[] = { "\cfb_skins\CH147_0.paa", "\cfb_skins\CH147_1.paa" };
+		};
+	};
 };
 
 class B_Heli_Transport_03_unarmed_F;
@@ -89,4 +135,16 @@ class CFB_Helo_Chinook_Unarmed : B_Heli_Transport_03_unarmed_F
 	vehicleClass = "CFB_RCAF_VehicleClass";
 	crew = "CFB_RCAF_Helo_Pilot";
 	hiddenSelectionsTextures[] = { "\cfb_skins\CH147_0.paa", "\cfb_skins\CH147_1.paa" };
+	
+	// This vehicle vehicle uses init[] script to automatically choose a skin from the texture sources
+	// (overwriting the hiddenSelectionsTextures[]). This forces it to choose one of the custom skins.
+	class TextureSources
+	{
+		class RCAF_Camo
+		{
+			displayName = "RCAF Camo";
+			factions[] = { "BLU_F" };
+			textures[] = { "\cfb_skins\CH147_0.paa", "\cfb_skins\CH147_1.paa" };
+		};
+	};
 };
